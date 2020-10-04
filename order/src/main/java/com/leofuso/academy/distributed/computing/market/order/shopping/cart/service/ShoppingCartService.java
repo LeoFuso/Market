9f68752523ade49e76deacd2088dfa7d18ee7759
Leonardo Nuzzo <leonardo.nuzzo@ifood.com.br>
2020-10-04 17:05:03 -0300

    Some more operations

--- order/src/main/java/com/leofuso/academy/distributed/computing/market/order/shopping/cart/service/ShoppingCartService.java
+++ order/src/main/java/com/leofuso/academy/distributed/computing/market/order/shopping/cart/service/ShoppingCartService.java
@@ -1,8 +1,10 @@
 package com.leofuso.academy.distributed.computing.market.order.shopping.cart.service;
 
 import com.leofuso.academy.distributed.computing.market.order.shopping.cart.application.action.AddCartItemRequest;
+import com.leofuso.academy.distributed.computing.market.order.shopping.cart.application.resource.ItemResource;
 import com.leofuso.academy.distributed.computing.market.order.shopping.cart.model.Cart;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ShoppingCartService {
@@ -11,4 +13,5 @@ public interface ShoppingCartService {
 
     Mono<Cart> addItem(Mono<AddCartItemRequest> request);
 
+    Flux<ItemResource> getItems(Long id);
 }
